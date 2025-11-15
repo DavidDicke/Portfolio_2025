7ed2da11915a7eb6e1fa1fb0e3e848897ac28af7
David Dicke <170459648+DavidDicke@users.noreply.github.com>
2025-11-15 21:52:01 +0100

    update video-controlls

--- app/javascript/controllers/pitch_video_controller.js
+++ app/javascript/controllers/pitch_video_controller.js
@@ -5,13 +5,15 @@ export default class extends Controller {
   connect() {
     // after 2 seconds the gif background image will be replaced with a jpg image, that play botton appears and pulses
     setTimeout(() => {
-      this.element.classList.remove('animated-img');
-      this.element.classList.add('static-img');
+      this.element.classList.remove('animated-greeting-img');
+      this.element.classList.add('animated-talking-img');
       this.element.querySelector('i').classList.remove('d-none');
     }, 2400);
   }
   startVideo() {
     this.element.querySelector('video').play();
+    this.element.classList.add('static-img');
+    this.element.classList.remove('animated-talking-img');
   }
   stopVideo() {
     this.element.querySelector('video').pause();
